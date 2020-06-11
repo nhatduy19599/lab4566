5ffa12715e55e39848d887192d219382154121c1
nhatduy19599 <nhatduy19599@gmail.com>
2020-06-11 14:05:55 +0700

    Add project files.

---
+++ LamNhatDuy_Lab456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LamNhatDuy_Lab456.MvcApplication" Language="C#" %>
---
+++ LamNhatDuy_Lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LamNhatDuy_Lab456.Startup))]
+namespace LamNhatDuy_Lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
